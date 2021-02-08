61f050c9ccfbac188fdcff7b08a9edb4f42e0051
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-02-08 17:33:05 -0500

    commit tree c9e19a5efc45d4525f7877d1950ee27c0de116e4

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+let test = [1, 2, 3, -9];
+sourceArray = [ false, null, null, null, true];
+
+function mapToNegativize(sourceArray){
+  return sourceArray.map(element => {return (element * -1);});
+};
+
+function mapToNoChange(sourceArray){
+  return sourceArray.map(el => {return el})
+};
+
+function mapToDouble(sourceArray){
+  return sourceArray.map(element => {return (element * 2)});
+};
+function mapToSquare(sourceArray){
+  return sourceArray.map(element => {return (element ** 2)});
+};
+
+function reduceToTotal(sourceArray, startingPoint = 0){
+  return sourceArray.reduce(function(memo, val){return memo + val}, startingPoint);
+};
+
+function reduceToAllTrue(sourceArray){
+  return sourceArray.reduce(function(memo, val){return (!!memo && !!val)});
+};
+
+function reduceToAnyTrue(sourceArray){
+  return sourceArray.reduce(function(memo, val){return (!!memo || !!val)}, false);
+};
